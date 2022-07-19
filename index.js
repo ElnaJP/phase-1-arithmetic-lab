3d77d983744bd4a1bc306a6244476329a143696b
Elna JP <eriosdd@gmail.com>
2022-07-18 20:48:51 -0400

    Assignment Completed

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
 
-let multiply = (num1,num2) =>
-{
-    return num1 * num2;
-}
-let resultMultiply = multiply(31*2)
+let random = Math.ceil(Number.isInteger(0));
 
+let num3 = 29;
+let num4 = 5 ;
+let mod = num3 % num4;
+
+let max = Math.max(1, 20)
